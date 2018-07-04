9ee8a2a2f0cbd598822938b86ece871149b37675
Taehyun Lim <dlatns0201@naver.com>
2018-07-05 02:34:18 +0900

    jade파일에서 CSS적용하는법을 찾고 commit

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 app.set('port', process.env.PORT || 3000);
-app.set('views', './views');
-app.set('view engine', 'jade');
+app.set('views','./views/template/assets/');
+app.set('view engine','jade');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 
-app.get('/',function(req,res){
-    res.render('main');
-})
+app.use(express.static('views/template'));
 
 app.listen(app.get('port'), function () {
     console.log("Server Connecting is Success with: ", app.get('port'));
-})
+});
