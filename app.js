ff8af60905223fd38a67dd6da82cc9dae74a3176
Taehyun Lim <dlatns0201@naver.com>
2018-07-16 02:28:16 +0900

    로그인에 세션을 사용 but 로그아웃의 오류문제(회원가입 후 로그아웃하고 로그인시 문제)

--- app.js
+++ app.js
@@ -1,57 +1,78 @@
-var express=require('express');
-var fs=require('fs');
-var bodyParser=require('body-parser');
-var app=express();
-var fs=require('fs');
-app.locals.pretty=true;
-app.set('port',process.env.PORT || 3000);
-app.set('views','./views');
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+var bkfd2Password = require('pbkdf2-password');
+var hasher = bkfd2Password();
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var LocalStrategy = require('passport-local').strategy;
+app.locals.pretty = true;
+var users = [];
+app.set('port', process.env.PORT || 3000);
+app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
-app.get('/',function(req,res){
-    res.render('index');
+app.use(session({
+    secret: '1q2w3e4r',
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore()
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function (req, res) {
+    if (req.session.username) {
+        res.render('index', { user: req.session.username });
+    }
+    else {
+        res.render('index');
+    }
 })
-app.get('/login', function(req,res){
+app.get('/login', function (req, res) {
     res.render('login');
 })
-app.get('/intro',function(req,res){
+app.get('/logout',function(req,res){
+    delete req.session.username;
+    res.redirect('/');
+})
+app.get('/intro', function (req, res) {
     res.render('intro');
 })
-app.get('/new',function(req,res){
+app.get('/new', function (req, res) {
     res.render('new');
 })
-app.post('/',function(req,res){
-    var id=req.body.id;
-    var pwd=req.body.pwd;
-    fs.readdir('views/data',function(err,files){
-        if(err){
-            res.status(500).send('잘못된 파일로드 입니다.');
+app.post('/login', function (req, res) {
+    var id = req.body.id;
+    var pwd = req.body.pwd;
+    for(var i=0; i<users.length; i++)
+    {
+        user=users[i];
+        if(id==user.id && pwd==user.pwd){
+            req.session.username=user.id;
+            return req.session.save(function(){
+                res.redirect('/');
+            });
         }
-        fs.readFile('views/data/'+id,'utf8',function(err,data){
-            if(err){
-                res.status(500).send('잘못된 회원 ID입니다.');
-            }
-            else if(data!=pwd){
-                res.status(500).send('잘못된 회원 PWD입니다.');
-            }
-            res.render('index',{user: id});
-        });
-
+    }
+});
+app.post('/new', function (req, res) {
+    var user = {
+        id: req.body.new_id,
+        pwd: req.body.new_pwd
+    };
+    users.push(user);
+    req.session.username = req.body.new_id;
+    req.session.save(function () {
+        res.redirect('/');
     });
-})
-app.post('/login',function(req,res){
-    var id=req.body.new_id;
-    var pwd=req.body.new_pwd;
-    fs.writeFile('views/data/'+id,pwd,function(err){
-        if(err){
-            res.send('데이터가 저장하는 경로가 없습니다');
-        }
-        res.render('login');
-    });
-})
+});
 
-app.listen(app.get('port'),function(){
-    console.log('Success Connected: '+app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Success Connected: ' + app.get('port'));
 })
